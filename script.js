bd19a1c3ba039e95578fd374f1b48a2b7e46ea46
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-16 11:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  let binary = '';
+  while (decimal > 0) {
+    // Divide the decimal number by 2 and get the remainder
+    const remainder = decimal % 2;
+    // Add the remainder to the binary string
+    binary = remainder + binary;
+    // Divide the decimal number by 2 to get the next quotient
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
 
-module.exports = threeSum;
+
+const decimal = 10;
+const binary = decimalToBinary(decimal);
+console.log(binary); // Output: 1010
+
